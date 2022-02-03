764e67b59ae63a939a4f3e610c64e3685b05a4c2
Bray Polkinghorne <braypolk@gmail.com>
2022-02-03 00:49:51 -0600

    able to get transactions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+
+// mainnet-beta, testnet, devnet
+let connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+
+
+
+const App = () => {
+  const [addr, setAddr] = useState('');
+  const [inputValue, setInputValue] = useState('2d9WL9hVev1ri7TMe4EYcP7CFgi47QsLX7u2JqRSPuyD');
+
+  const onInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
+
+  const getDetails = async (pubkey) => {
+    let limit = 1000
+    let transSig = ''
+    let params = { 'limit': limit }
+    let account = await connection.getSignaturesForAddress(pubkey, params);
+    console.log(account);
+
+    // while (account.length = limit) {
+    //   transSig = account.at(-1).signature
+    //   params = { 'before': transSig, 'limit': limit }
+    //   account = await connection.getSignaturesForAddress(pubkey, params);
+    //   console.log(account);
+    // }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          console.log(e.target[0].value);
+          const pubkey = new PublicKey(e.target[0].value)
+          getDetails(pubkey)
+        }}>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={onInputChange}
+          />
+          <input type="submit" />
+        </form>
       </header>
     </div>
   );
