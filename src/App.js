b1f636c81e7e44968a67d7402c51f3fe65c181bc
Bray Polkinghorne <braypolk@gmail.com>
2022-02-04 09:15:54 -0600

    transaction details

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import { useEffect, useState } from 'react';
 import logo from './logo.svg';
+import Transactions from './test-transactions.json'
+import Details from './test-transaction-detail.json'
 import './App.css';
 
+import { useEffect, useState } from 'react';
 import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
 
+// https://token-list.solana.com/solana.tokenlist.json list of lots of token names/pairs/symbols
+
 // mainnet-beta, testnet, devnet
-let connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
-
-
+let connection = new Connection(clusterApiUrl('mainnet-beta'), 'confirmed');
 
 const App = () => {
-  const [addr, setAddr] = useState('');
+  const [owner, setOwner] = useState('');
   const [inputValue, setInputValue] = useState('2d9WL9hVev1ri7TMe4EYcP7CFgi47QsLX7u2JqRSPuyD');
 
   const onInputChange = (event) => {
@@ -33,12 +35,52 @@ const App = () => {
     // }
   }
 
+  const getTransactionDetail = async (transaction) => {
+    let transactionDetail = await connection.getConfirmedTransaction(transaction);
+    console.log(transactionDetail)
+  }
+  
+  // useEffect(() => {
+    // getTransactionDetail(Transactions[0].signature)
+  // })
+  let post, pre, token
+  // pre token owner balance
+  Details.meta.preTokenBalances.forEach(b => {
+    if(b.owner == inputValue){
+      console.log(b.uiTokenAmount.uiAmount + " " + b.mint.substring(0,4)) //TODO: get actual token through list lookup
+      pre = b.uiTokenAmount.uiAmount == null ? 0 : b.uiTokenAmount.uiAmount
+      token = b.mint.substring(0,4)
+    }
+  })
+  // post token owner balance
+  Details.meta.postTokenBalances.forEach(b => {
+    if(b.owner == inputValue){
+      console.log(b.uiTokenAmount.uiAmount + " " + b.mint.substring(0,4)) //TODO: get actual token through list lookup
+      post = b.uiTokenAmount.uiAmount == null ? 0 : b.uiTokenAmount.uiAmount
+    }
+  })
+  
+  let finalBalance = post - pre
+  console.log(finalBalance);
+
+  if(finalBalance > 0) {
+    console.log("You bought " + finalBalance + " " + token);
+  }
+  else {
+    console.log("You sold " + finalBalance + " " + token);
+  }
+
+  // TODO: next steps will be to figure out different transactions and what steps they have
+  // ex: staking has different options than trading usdc for sol
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <form onSubmit={(e) => {
+        {/* <form onSubmit={(e) => {
           e.preventDefault();
           console.log(e.target[0].value);
+          setOwner(e.target[0].value)
           const pubkey = new PublicKey(e.target[0].value)
           getDetails(pubkey)
         }}>
@@ -48,7 +90,11 @@ const App = () => {
             onChange={onInputChange}
           />
           <input type="submit" />
-        </form>
+        </form> */}
+        {/* {Transactions.map(transaction => (
+          <li key={transaction.signature}>{transaction.signature}</li>
+        ))} */}
+        {/* {getTransactionDetail(Transactions[0].signature)} */}
       </header>
     </div>
   );
